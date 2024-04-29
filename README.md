e6ccb4f5300b0247ec379ae9da9627f103338413
Guillermo Mazzola <gmazzo65@gmail.com>
2024-04-29 14:26:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Sample repo of https://github.com/gmazzo/kmp-compose-issue-4444
+
+Add any `BuildConfig` field in the `composeApp` module to trigger it.
---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    // this is necessary to avoid the plugins to be loaded multiple times
+    // in each subproject's classloader
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.jetbrainsCompose) apply false
+    alias(libs.plugins.kotlinMultiplatform) apply false
+    id("com.github.gmazzo.buildconfig") version "5.3.5-26-gd9590d5" apply false
+}
